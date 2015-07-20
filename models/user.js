6028862d8fc3969ce750270f8f25a10793ad9ce8
rchau05 <rnchau@mail.sfsu.edu>
2015-07-20 15:00:45 -0700

    stuff

--- models/user.js
+++ models/user.js
@@ -2,7 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var RestSchema = new Schema({
-	name: String
+	name: String,
+	location: String,
+	price: Number
 });
 
 module.exports = mongoose.model('restaurant', RestSchema);
+
+
+// client make request to server to external app
+// and back to server and back to client
+
+// $.get('url')
