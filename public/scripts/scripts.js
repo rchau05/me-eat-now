0ab1782b6167cb63249a6b3a79f02a0b6cd0ae98
rchau05 <rnchau@mail.sfsu.edu>
2015-07-21 15:13:58 -0700

    added api

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,20 +1,47 @@
 $(function() {
 
-var restTemplate = _.template($('#rest-template').html());
+	var restTemplate = _.template($('#rest-template').html());
 
-var restaurants = [
-	{name: "Restaurant1", location:"123 Fake St."},
-	{name: "Restaurant2", location:"456 Faker St."},
-	{name: "Restaurant3", location:"789 Fakest St."},
-	{name: "Restaurant4", location:"000 Fakerest St."},
-	{name: "Restaurant5", location:"666 Fakestest St."},
-	{name: "Restaurant6", location:"042 Fakest infinity + 1 St."}
-];
+	// var restaurants = [
+	// 	{name: "Restaurant1", location:"123 Fake St."},
+	// 	{name: "Restaurant2", location:"456 Faker St."},
+	// 	{name: "Restaurant3", location:"789 Fakest St."},
+	// 	{name: "Restaurant4", location:"000 Fakerest St."},
+	// 	{name: "Restaurant5", location:"666 Fakestest St."},
+	// 	{name: "Restaurant6", location:"042 Fakest infinity + 1 St."}
+	// ];
 
-_.each(restaurants, function (restaurant, index) {
-	var $restList = $(restTemplate(restaurant));
-	$restList.attr('data-index', index);
-	$('#rest-list').append($restList);
-});
+	$.get('/api/restaurants', function(data) {
+		_.each(data.businesses, function (restaurant, index) {
+			console.log(restaurant);
+			var $restList = $(restTemplate(restaurant));
+			$restList.attr('data-index', index);
+			$('#rest-list').append($restList);
+		});
+		console.log(restaurants)
+	});
+
+
+	// var options = {
+	//   enableHighAccuracy: true,
+	//   timeout: 5000,
+	//   maximumAge: 0
+	// };
+
+	// function success(pos) {
+	//   var crd = pos.coords;
+
+	//   console.log('Your current position is:');
+	//   console.log('Latitude : ' + crd.latitude);
+	//   console.log('Longitude: ' + crd.longitude);
+	//   console.log('More or less ' + crd.accuracy + ' meters.');
+	// };
+
+	// function error(err) {
+	//   console.warn('ERROR(' + err.code + '): ' + err.message);
+	// };
+
+	// navigator.geolocation.getCurrentPosition(success, error, options);
+
 
 });
--- server.js
+++ server.js
@@ -20,102 +20,25 @@ mongoose.connect(
 // var Restaurant = require('./models/user');
 
 // // serve js and css files from public 
-app.use(express.static(__dirname + '/public/views'));
+app.use(express.static(__dirname + '/public'));
 
 // // tell app to use bodyParser middleware
 // // this will let us get data from POST
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//routes for api
-var router = express.Router();
-
-router.use(function(req, res, next) {
-	//do logging
-	console.log('Something is happening');
-	next();
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/views/index.html')   
 });
 
-router.get('/', function(req, res) {
-    res.sendFile('/index.html');   
-});
-
-app.get('/yolo', function(req, res){
+app.get('/api/restaurants', function(req, res){
 	yelp.search({term: 'pizza', location: "San Francisco"}, function(error, data) {
 		res.json(data);
 	});
+
 	// res.sendFile(__dirname + '/public/views/welcome.html');
 });
 
-router.route('/restaurants') 
-	//create a bear assessed at POST localhost:8080
-	.post(function(req, res) {
-		var restaurant = new Restaurant();
-		restaurant.name = req.body.name,
-		restaurant.location = req.body.location,
-		restaurant.price = req.body.price;
-
-		restaurant.save(function(err) {
-			if (err)
-				res.send(err);
-
-			res.json({message: 'Restaurant listed!'});
-		});
-	});
-
-router.route('/restaurants')
-	//get all restaurant listings
-	.get(function(req, res) {
-		Restaurant.find(function(err, restaurants) {
-			if(err)
-				res.send(err);
-
-			res.json(restaurants);
-		});
-	});
-
-router.route('/restaurants/:restaurant_id')
-//getting one restaurant
-	.get(function(req, res) {
-	
-	Restaurant.findById(req.params.restaurant_id, function(err, restaurant) {
-		if(err);
-			res.send(err);
-			res.json(restaurant);
-		});
-	})
-	.put(function(req, res) {
-
-	Restaurant.findById(req.params.bear_id, function(err, restaurant) {
-		if (err);
-			res.send(err);
-
-		restaurant.name = req.body.name,
-		restaurant.location = req.body.location,
-		restaurant.price = req.body.price
-
-		//save the restaurant
-		restaurant.save(function(err) {
-			if(err)
-				res.send(err)
-
-		res.json({message: 'restaurant updated!'})
-	})
-  })
-	.delete(function(req, res) {
-		Restaurant.remove({
-			_id: req.params.restaurant_id
-		}, function(err, restaurant) {
-			if(err)
-				res.send(err);
-
-			res.json({message: 'Successfully deleted'})
-		})
-	})
-})
-	
-// //register our routes
-app.use('/api', router)
 
 app.listen(process.env.PORT || 3000, function(){
 console.log('It works!')
