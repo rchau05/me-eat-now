58ae79d6c7d35c51b0877f587b9206f9518ec6de
rchau05 <rnchau@mail.sfsu.edu>
2015-07-23 23:47:34 -0700

    removed some files

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	PORT: process.env.PORT || '3000',
-	MONGO_URI: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/users',
-	SESSION_SECRET: process.env.SESSION_SECRET || 'GreenSecret'
-}
